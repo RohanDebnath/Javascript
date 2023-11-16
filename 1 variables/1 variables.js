1bdc4643e49b866722b359b4867a0a7ee71115ae
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-11-16 06:22:15 +0530

    variables done

---
+++ 1 variables/1 variables.js
@@ -0,0 +1,7 @@
+let Fname="Rohan"
+let age = 22
+let isSchool=false
+
+document.getElementById("p1").textContent=Fname
+document.getElementById("p2").textContent=age
+document.getElementById("p3").textContent=isSchool
