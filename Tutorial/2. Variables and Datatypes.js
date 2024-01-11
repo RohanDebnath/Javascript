4bbb3be0677da3b0229d476cd7700d92467cc6ea
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2024-01-11 23:30:26 +0530

    Veriables and Datatypes done

---
+++ Tutorial/2. Variables and Datatypes.js
@@ -0,0 +1,7 @@
+let fristName="Rohan";
+let age=22;
+let inCollege=true;
+
+document.getElementById("p1").innerHTML="Hello " + fristName;
+document.getElementById("p2").innerHTML="You are " + fristName;
+document.getElementById("p3").innerHTML="College Student? " + fristName;
