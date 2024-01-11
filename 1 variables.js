bf5a9ba1c6a352005cb3a70f34a19102660cfc94
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2024-01-11 23:09:06 +0530

    Starting Fresh

--- 1 variables.js
+++
@@ -1,7 +0,0 @@
-let Fname="Rohan"
-let age = 22
-let isSchool=false
-
-document.getElementById("p1").textContent=Fname
-document.getElementById("p2").textContent=age
-document.getElementById("p3").textContent=isSchool
