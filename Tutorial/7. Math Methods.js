594ce1d97711d5f27392f264a13bc7decadb286c
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2024-01-12 21:12:44 +0530

    Math Method done

---
+++ Tutorial/7. Math Methods.js
@@ -0,0 +1,25 @@
+let x=10;
+let y=12;
+let pi=3.142;
+
+let maximum;
+let minimun;
+
+pi=Math.round(pi);
+console.log(pi);
+pi=Math.floor(pi);
+console.log(pi);
+pi = Math.ceil(pi);
+console.log(pi);
+x = Math.pow(x, 2);
+console.log(x);
+y = Math.sqrt(y);
+console.log(y);
+x = Math.abs(x);
+console.log(x);
+maximum = Math.max(x, y, pi);
+console.log(maximum);
+minimum = Math.min(x, y, 1);
+console.log(minimun);
+x = Math.PI;
+console.log(x);
