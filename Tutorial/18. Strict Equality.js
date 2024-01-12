842aca9a76619a9a76cc74091ec7fc2e75ac3255
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2024-01-13 00:06:43 +0530

    strict equality operator

---
+++ Tutorial/18. Strict Equality.js
@@ -0,0 +1,14 @@
+// = assignment operator
+// ==comparison operator
+// === strict equal operator (compare if values and datatypes are equal)
+// != inequality operator
+// !=== strict inequality operator
+
+let PI=3.142;
+console.log(typeof PI);
+
+console.log(PI==3.142);
+console.log(PI=="3.142");
+
+console.log(PI===3.142);
+console.log(PI==="3.142");
