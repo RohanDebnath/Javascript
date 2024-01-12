9d744112c63c97214985f8d65ac2ec3a8e76d266
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2024-01-12 23:02:47 +0530

    slice of string done

---
+++ Tutorial/12. Slice Mthod.js
@@ -0,0 +1,5 @@
+let fullName="Rohan Debnath";
+let lastName=fullName.slice(fullName.indexOf(" ")+1);
+console.log(lastName);
+let firstName=fullName.slice(0,fullName.indexOf(" "));
+console.log(firstName);
