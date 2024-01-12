e29531ea3cd5f9c183036b84c81c8c741091482d
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2024-01-12 20:09:35 +0530

    Type conversion done

---
+++ Tutorial/5. TypeConversion.js
@@ -0,0 +1,28 @@
+// let age = window.prompt("How old are you");
+// age+=1;
+// document.getElementById("p1").innerHTML="Happy birthday you are "+ age +" years old";
+// document.getElementById("p2").innerHTML=typeof age;
+// console.log(age);
+// age=Number(age);
+// age+=1;
+// document.getElementById("p3").innerHTML="Happy bday you are "+ age + " years old"
+// console.log(age);
+
+let age = window.prompt("How old are you?");
+
+console.log(typeof age);
+age = Number(age);
+age += 1;
+
+console.log("Happy Birthday! You are", age, "years old");
+let x;
+let y;
+let z;
+
+x = Number("pizza");
+y = String(3.14);
+z = Boolean("pizza");
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
